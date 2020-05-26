6d07abbe93bacd4b5c53cdc699255bbbb2b63d5d
ismet <ismet.kamal@yahoo.com>
2020-05-26 07:17:04 -0400

    update

--- ConsoleApp/ArrayKthLargestElement.cs
+++ ConsoleApp/ArrayKthLargestElement.cs
@@ -8,16 +8,16 @@ namespace ConsoleApp
 {
     class ArrayKthLargestElement
     {
-        static void Main()
-        {
-            ArrayKthLargestElement l = new ArrayKthLargestElement();
-            int[] arr = new int[] {3, 2, 1, 5, 6, 4 };
-            int k = 2;
-            int kth = l.findKthLargest(arr, k);
-            arr.ToList().ForEach(x => Console.Write(x + " "));
-            Console.WriteLine($"\nArray {k}th Largest Element: {kth}");            
-            Console.ReadKey();
-        }
+        //static void Main()
+        //{
+        //    ArrayKthLargestElement l = new ArrayKthLargestElement();
+        //    int[] arr = new int[] {3, 2, 1, 5, 6, 4 };
+        //    int k = 2;
+        //    int kth = l.findKthLargest(arr, k);
+        //    arr.ToList().ForEach(x => Console.Write(x + " "));
+        //    Console.WriteLine($"\nArray {k}th Largest Element: {kth}");            
+        //    Console.ReadKey();
+        //}
 
         int findKthLargest(int[] arr, int k)
         {
---
+++ ConsoleApp/BinaryTreeTraversal.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp
+{
+    class BinaryTreeTraversal
+    {
+        static void Main()
+        {
+            BinaryTreeTraversal t = new BinaryTreeTraversal();
+            //    1
+            //     \
+            //      2
+            //     /
+            //    3
+            // post-order: 3, 2, 1
+            //TreeNode root = new TreeNode(1);
+            //root.right = new TreeNode(2);
+            //root.right.left = new TreeNode(3);
+
+            //    1
+            //   / \
+            //  0   2
+            //     / \
+            //    3   4
+            // post-order: 0, 3, 4, 2, 1
+            TreeNode root = new TreeNode(1);
+            root.right = new TreeNode(2);
+            root.left = new TreeNode(0);
+            root.right.left = new TreeNode(3);
+            root.right.right = new TreeNode(4);
+
+            List<int> po = t.PostorderTraversal(root);
+            Console.WriteLine($"Postorder Traversal: ");
+            po.ForEach(x => Console.Write(x + " "));
+            Console.WriteLine($"");
+            Console.ReadKey();
+        }
+
+        // iterative approach
+        public List<int> PostorderTraversal(TreeNode root)
+        {
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            List<int> result = new List<int>();
+            for (TreeNode lastNode = null, peek; root != null || stack.Count != 0;)
+                if (root != null)
+                {
+                    stack.Push(root);
+                    root = root.left;
+                }
+                else if ((peek = stack.Peek()).right != null && peek.right != lastNode)
+                    root = peek.right;
+                else
+                {
+                    result.Add(peek.val);
+                    lastNode = stack.Pop();
+                }
+            return result;
+        }
+
+        // simpler version using pre-order traversal and reversing the result upon return.
+        public List<int> PostorderTraversal2(TreeNode root)
+        {
+            var result = new List<int>();
+
+            if (root == null) return result;
+
+            var stack = new Stack<TreeNode>();
+            stack.Push(root);
+
+            while (stack.Any())
+            {
+                var cur = stack.Pop();
+                result.Add(cur.val);
+
+                if (cur.left != null)
+                {
+                    stack.Push(cur.left);
+                }
+
+                if (cur.right != null)
+                {
+                    stack.Push(cur.right);
+                }
+            }
+
+            result.Reverse();
+
+            return result;
+        }
+
+
+        public List<int> PostorderTraversal3(TreeNode root) //Recursive 
+        {
+            List<int> res = new List<int>();
+            Recursive(root, res);
+            return res;
+        }
+
+        public void Recursive(TreeNode node, List<int> res)
+        {
+            if (node == null) return;
+            Recursive(node.left, res);
+            Recursive(node.right, res);
+            res.Add(node.val);
+        }
+
+        public List<int> PostorderTraversal4(TreeNode root)
+        {
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            Stack<int> res = new Stack<int>();
+            if (root != null) stack.Push(root);
+            while (stack.Count > 0)
+            {
+                TreeNode node = stack.Pop();
+                res.Push(node.val);
+                if (node.left != null) stack.Push(node.left);
+                if (node.right != null) stack.Push(node.right);
+            }
+            return res.ToList();
+        }
+
+        public class TreeNode
+        {
+            public int val;
+            public TreeNode left;
+            public TreeNode right;
+            public TreeNode(int v)
+            {
+                val = v;
+            }
+        }
+
+    }
+}
